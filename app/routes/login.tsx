c7ace2cc900c3b5ca105166e7853902e2eadf033
Paul DeVito <pdevito3@gmail.com>
2022-06-29 09:44:09 -0400

    update: file didn't save

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
   return (
-    <form action="/auth/duende" method="post">
-      <button>Login with Duende</button>
+    <form action="/auth/keycloak" method="post">
+      <button>Login with Keycloak</button>
     </form>
   );
 }
